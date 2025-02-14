10a287dddc5a732fd203dbed793d886c6f27dfbd
Francesc Bastida <franbastida@gmail.com>
2025-02-14 11:32:54 +0100

    fix

--- modules/docbaseCountProcess.sh
+++ modules/docbaseCountProcess.sh
@@ -18,58 +18,51 @@ docbaseCountProcess () {
     local _ARRAY_NORMAL=()
     local _ARRAY_ERROR=()
     local _ARRAY_DOCBASES=()
+    local _ARRAY_FAIL_DOCBASES=()
     inputParameterHandler "$@"
 ######################################################################
 
 ### Main body
-    if [ ${#_DOCBASES[@]} -eq 0 ]; then
-        # Get list of all docbases on the server
-        _DOCBASES=(${_DOCBASES:-`ls -1 $DOCUMENTUM/dba/config/`})
+    if [ ${#_ARRAY_DOCBASES[@]} -eq 0 ]; then
+        # Get list of all docbases on the server with teh expression :-
+        _ARRAY_DOCBASES=(${_ARRAY_DOCBASES:-`ls -1 $DOCUMENTUM/dba/config/`})
     fi
-    if [ $DEBUG == 1 ]; then outputHandler "Running documentum process test of docbases: '${_DOCBASES[*]}'" "DEBUG" "$_APPLICATION" "$_OBJECT"; fi
+    
+    if [ $DEBUG == 1 ]; then outputHandler "Running documentum process test of docbases: '${_ARRAY_DOCBASES[*]}'" "DEBUG" "$_APPLICATION" "$_OBJECT"; fi
 
-    local _RESULTDOCUMENTUMnotRUNNING=0;
+    local _MSG="No documentum processes found for at least one docbase"
 
-    for _docbase in "${_DOCBASES[@]}";
+    for _docbase in "${_ARRAY_DOCBASES[@]}";
     do
         if [ $DEBUG == 1 ]; then outputHandler "Running documentum process test for docbase: '${_docbase}'" "DEBUG" "$_APPLICATION" "$_OBJECT"; fi
 
         # Count processes for docbase
         local _DOCBASE_PROC_COUNT=`ps -fu $DMUSER | grep "./documentum -docbase_name $_docbase" |  grep -v grep | wc -l`
-        if [ $TRACE == 1 ]; then outputHandler "_DOCBASE_AGENT_EXEC_PROC_COUNT for '$_docbase': $_DOCBASE_PROC_COUNT" "TRACE" "$_APPLICATION" "$_OBJECT"; fi
 
         if [[ $_DOCBASE_PROC_COUNT -lt 1 ]]; then
-            _RESULTDOCUMENTUMnotRUNNING=$_RESULTDOCUMENTUMnotRUNNING+1;    
-        fi
-
-        # Set the message for notification subsystem
-        local _MSG="No documentum processes found for $_docbase"
-        if [ $TRACE == 1 ]; then outputHandler "_MSG: '${_MSG}'" "TRACE" "$_APPLICATION" "$_OBJECT"; fi
-        
-        if [[ $_DOCBASE_PROC_COUNT -lt 1 ]]; then
-            outputHandler "$_MSG" "$_SEVERITY" "$_APPLICATION" "$_OBJECT" # Echo error message to logs/output
+            # Proceed with notification ERROR
+            local _MSG_error="ERROR -> No documentum processes found for $_docbase"
+            outputHandler "$_MSG_error" "$_SEVERITY" "$_APPLICATION" "$_OBJECT" # Echo error message to logs/output
+            _ARRAY_FAIL_DOCBASES+=($_docbase)
         else
             outputHandler "$_DOCBASE_PROC_COUNT documentum processes are running for $_docbase" "INFO" "$_APPLICATION" "$_OBJECT"  # Echo info message to logs/output
         fi
      done
      
     #Processing the errors/normal arrays
-
-     _MSG="No documentum processes found for at least one docbase"
-        
-     if [[ $_RESULTDOCUMENTUMnotRUNNING -lt 1 ]]; then
+     if [[ ${#_ARRAY_FAIL_DOCBASES[*]} -lt 1 ]]; then
         # Process the normal path
         _ARRAY_NORMAL[${#_ARRAY_NORMAL[@]}]="$_MSG|" # Add error to array with items without error
      else
         # Process the error state path
-        outputHandler "$_MSG" "$_SEVERITY" "$_APPLICATION" "$_OBJECT" # Echo error message to logs/output
+        outputHandler "No documentum processes found for at least one docbase ${_ARRAY_FAIL_DOCBASES[*]}" "$_SEVERITY" "$_APPLICATION" "$_OBJECT" # Echo error message to logs/output
         _ARRAY_ERROR[${#_ARRAY_ERROR[@]}]="$_MSG|" # Add error to array with errors
      fi
      
-     # Variable cleanup for loop
      unset _MSG
+     unset _MSG_error
+     unset _ARRAY_DOCBASES
      unset _DOCBASE_PROC_COUNT
-     unset _RESULTDOCUMENTUMnotRUNNING
    
 
 ######################################################################
